104bb71b424590616ba4be72b78de0983402a9f8
volnysensyo <70943945+volnysensyo@users.noreply.github.com>
2021-05-13 16:30:22 +0900

    OOO

--- contact/index.php
+++ contact/index.php
@@ -11,9 +11,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;200;300;400;500&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/sensyo.css">
+    <link rel="stylesheet" href="../sensyo.css">
 
-<link rel="stylesheet" href="/betternav.css">
+<link rel="stylesheet" href="../betternav.css">
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174508777-1"></script>
     <script>
@@ -28,6 +28,7 @@
 </head>
 
 <body class="japanese">
+
 
     <!-- TOP BANNER -->
     <div class="alert alert-warning alert-dismissible fade show information visually-hidden" role="alert">
@@ -628,9 +629,10 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
@@ -641,9 +643,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
         crossorigin="anonymous"></script>
-    <script src="/js.js"></script>
+    <script src="../js.js"></script>
 
-    <script src="/aos.js"></script>
+    <script src="../aos.js"></script>
 
     <script>
         AOS.init();
